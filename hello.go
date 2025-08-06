c30494cef27f6f78ac0572d0942027e5fc36451f
xuanhung <xuanhung3081330@gmail.com>
2025-08-06 11:32:15 +0900

    Playing with for loop and benchmark in testing

--- hello.go
+++ hello.go
@@ -2,14 +2,31 @@ package main
 
 import "fmt"
 
-func Hello(name string) string {
+func Hello(name string, language string) string {
 	if name == "" {
 		name = "World"
 	}
 
+	if language == "Spanish"{
+		return "Hola, " + name
+	}
+
+	if language == "French" {
+		return "Bonjour, " + name
+	}
+
+
 	return "Hello, " + name
 }
 
+func Repeat(character string, loop_time int)  (result string) {
+	for i := 0; i < loop_time; i++ {
+		result += character
+	}
+
+	return result;
+}
+
 func main(){
-	fmt.Println(Hello("Hung"))
+	fmt.Println(Hello("Hung", ""))
 }
--- hello_test.go
+++ hello_test.go
@@ -4,18 +4,42 @@ import "testing"
 
 func TestHello(t *testing.T){
 	t.Run("saying hello to people", func(t *testing.T){
-		got := Hello("Hung")
+		got := Hello("Chris", "")
 		want := "Hello, Chris"
 
 		assertCorrectMessage(t, got, want)
 	})
 
 	t.Run("say 'Hello, World' when an empty string is supplied", func(t *testing.T){
-		got := Hello("")
+		got := Hello("", "")
 		want := "Hello, World"
 
 		assertCorrectMessage(t, got, want)
 	})
+
+	t.Run("in Spanish", func(t *testing.T){
+		got := Hello("Elodie", "Spanish")
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+
+	t.Run("in French", func(t *testing.T){
+		got := Hello("Hung", "French")
+		want := "Bonjour, Hung"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func TestRepeat(t *testing.T){
+	got := Repeat("a", 5)
+	want := "aaaaa"
+	assertCorrectMessage(t, got, want)
+}
+
+func BenchmarkRepeat(b *testing.B){
+	for b.Loop(){
+		Repeat("a", 100)
+	}
 }
 
 func assertCorrectMessage(t testing.TB, got, want string) {
