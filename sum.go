ef4e1340abc4a94e801d12e6267122d87484670d
xuanhung <xuanhung3081330@gmail.com>
2025-08-06 13:54:02 +0900

    Played with some slice functions

--- sum.go
+++ sum.go
@@ -8,3 +8,13 @@ func Sum(numbers []int) int {
 
 	return sum;
 }
+
+func SumAll(numbersToSum ...[]int) []int{
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum(numbers))
+	}
+
+	return sums
+}
