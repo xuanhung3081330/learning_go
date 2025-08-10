9273b6706a263c60eff632816b345da6ff4f5243
xuanhung <xuanhung3081330@gmail.com>
2025-08-10 18:04:10 +0900

    Further refactored testing

--- perimeter_test.go
+++ perimeter_test.go
@@ -15,21 +15,19 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
-	checkArea := func(t testing.TB, shape Shape, want float64){
-		t.Helper()
-		got := shape.Area()
-		if got != want {
-			t.Errorf("got %g want %g", got, want)
-		}
+	areaTests := []struct {
+		shape Shape
+		want float64
+	}{
+		{shape: Rectangle{Width: 12, Height: 6}, want: 72.0},
+		{shape: Circle{Radius: 10}, want: 314.1592653589793},
+		{shape: Triangle{Base: 12, Height: 6}, want: 36.0},
 	}
 
-	t.Run("rectangles", func(t *testing.T) {
-		rectangle := Rectangle{12.0, 6.0}
-		checkArea(t, rectangle, 72.0)
-	})
-
-	t.Run("circles", func(t *testing.T) {
-		circle := Circle{10}
-		checkArea(t, circle, 314.1592653589793)
-	})
+	for _, tt := range areaTests {
+		got := tt.shape.Area()
+		if got != tt.want {
+			t.Errorf("got %g want %g", got, tt.want)
+		}
+	}
 }
