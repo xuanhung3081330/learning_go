7985a891199ffb029a5a611b2aab1b6b2580e494
xuanhung <xuanhung3081330@gmail.com>
2025-08-10 17:50:39 +0900

    Used interface for general 'shape'

--- perimeter_test.go
+++ perimeter_test.go
@@ -15,11 +15,21 @@ func TestPerimeter(t *testing.T){
 }
 
 func TestArea(t *testing.T){
-	rectangle := Rectangle{12.0, 6.0}
-	got := Area(rectangle)
-	want := 72.0
-
-	if got != want {
-		t.Errorf("got %.2f want %.2f", got, want)
+	checkArea := func(t testing.TB, shape Shape, want float64){
+		t.Helper()
+		got := shape.Area()
+		if got != want {
+			t.Errorf("got %g want %g", got, want)
+		}
 	}
+
+	t.Run("rectangles", func(t *testing.T) {
+		rectangle := Rectangle{12.0, 6.0}
+		checkArea(t, rectangle, 72.0)
+	})
+
+	t.Run("circles", func(t *testing.T) {
+		circle := Circle{10}
+		checkArea(t, circle, 314.1592653589793)
+	})
 }
