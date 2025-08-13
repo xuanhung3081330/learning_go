09e85d639a8bbe6e142d997bd2a7798cfe6efecf
xuanhung <xuanhung3081330@gmail.com>
2025-08-13 22:17:19 +0900

    Swap repo

--- di_in_go.go
+++ di_in_go.go
@@ -22,19 +22,26 @@ import "fmt"
 //	}
 //}
 
-// Interface
+// Step 1: Interface
 type UserRepo interface {
 	GetUser(id int) string
 }
 
-// Real implementation
+// Step 2: Real implementation
 type PostgresUserRepo struct {}
 
 func (r PostgresUserRepo) GetUser(id int) string {
 	return fmt.Sprintf("User%d from Postgres", id)
 }
 
-// Service
+// Step 3: Mock implementation (for testing)
+type MockUserRepo struct {}
+
+func (m MockUserRepo) GetUser(id int) string {
+	return fmt.Sprintf("Mocked user %d", id)
+}
+
+// Step 4: Service that depends on the UserRepo interface
 type UserService struct {
 	repo UserRepo
 }
@@ -49,8 +56,15 @@ func (s *UserService) PrintUser(id int) {
 	fmt.Println(s.repo.GetUser(id))
 }
 
+// Step 5: main (Manual DI)
 func main(){
+	// In production
 	repo := PostgresUserRepo{}
 	service := NewUserService(repo) // Inject dependency
 	service.PrintUser(2)
+
+	// In testing (swap dependency)
+	mockRepo := MockUserRepo{}
+	mockService := NewUserService(mockRepo)
+	mockService.PrintUser(4)
 }
