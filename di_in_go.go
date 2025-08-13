ef0964c43637cc9863106bab62ac82eff8ab9e0f
xuanhung <xuanhung3081330@gmail.com>
2025-08-13 22:08:00 +0900

    Created a complete DI example

--- di_in_go.go
+++ di_in_go.go
@@ -2,22 +2,55 @@ package main
 
 import "fmt"
 
-type Service interface {
-	DoSomething()
+//type Service interface {
+//	DoSomething()
+//}
+
+//type RealService struct{}
+
+//func (s RealService) DoSomething() {
+//	fmt.Println("Doing something real")
+//}
+
+//type Controller struct {
+//	service Service
+//}
+
+//func NewController(s Service) *Controller {
+//	return &Controller{
+//		service: s,
+//	}
+//}
+
+// Interface
+type UserRepo interface {
+	GetUser(id int) string
 }
 
-type RealService struct{}
+// Real implementation
+type PostgresUserRepo struct {}
 
-func (s RealService) DoSomething() {
-	fmt.Println("Doing something real")
+func (r PostgresUserRepo) GetUser(id int) string {
+	return fmt.Sprintf("User%d from Postgres", id)
 }
 
-type Controller struct {
-	service Service
+// Service
+type UserService struct {
+	repo UserRepo
 }
 
-func NewController(s Service) *Controller {
-	return &Controller{
-		service: s,
+func NewUserService(r UserRepo) *UserService {
+	return &UserService{
+		repo: r,
 	}
+}
+
+func (s *UserService) PrintUser(id int) {
+	fmt.Println(s.repo.GetUser(id))
+}
+
+func main(){
+	repo := PostgresUserRepo{}
+	service := NewUserService(repo) // Inject dependency
+	service.PrintUser(2)
 }
